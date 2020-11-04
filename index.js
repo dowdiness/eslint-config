46b99f8d77ed30b411fa4b55f1a2cdbd7257e603
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-11-04 17:45:44 +0900

    use standard instead of standard-with-typescript

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true
   },
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
-    "ecmaFeatures": {
-      "jsx": true
+    'ecmaFeatures': {
+      'jsx': true
     },
     sourceType: 'module',
     project: './tsconfig.json'
@@ -21,6 +21,16 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   rules: {
+    'quotes': 0,
+    'semi': ['error', 'never', {'beforeStatementContinuationChars': 'never'}],
+    'semi-spacing': ['error', {'after': true, 'before': false}],
+    'semi-style': ['error', 'first'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/prop-types': 'off',
   }
-};
+}
